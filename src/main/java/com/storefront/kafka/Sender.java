c259194939f9e2dd60b66b6d23ed2c25f538f8f3
Gary A. Stafford <garystafford@rochester.rr.com>
2018-06-03 16:39:54 -0400

    Fix messaging, refactor entities

--- src/main/java/com/storefront/kafka/Sender.java
+++ src/main/java/com/storefront/kafka/Sender.java
@@ -3,7 +3,6 @@ package com.storefront.kafka;
 import com.storefront.model.OrderStatusEventChange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j
