4d3cdd02373b2e2019d35f9e8583c8b008cab1a9
Gary A. Stafford <garystafford@rochester.rr.com>
2018-06-18 22:45:49 -0400

    Enable discovery

--- src/main/java/com/storefront/Application.java
+++ src/main/java/com/storefront/Application.java
@@ -2,7 +2,12 @@ package com.storefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * author Gary A. Stafford
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
